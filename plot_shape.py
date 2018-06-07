a3fb157fb234a7641055b0ae781cabad1cddc018
Cristian Duartes <c.duartes.dn@gmail.com>
2018-06-06 20:25:54 -0400

    re-structure functions

--- plot_shape.py
+++ plot_shape.py
@@ -6,10 +6,12 @@ from matplotlib.path import Path
 import math
 
 def get_intersection(f,s):
+    '''
+    Return a new patches.Rectangle with the intersection between two patches.Rectangle (f and s parameters) there's one.
+    '''
     path_f = Path(f.get_verts())
     path_s = Path(s.get_verts())
     pc = path_f.intersects_path(path_s, filled = False)
-    #print('path intersection', pc)
     if pc:
         this_boxes = [f,s]
         min_x = min(f.get_verts()[0][0],s.get_verts()[0][0])
@@ -24,18 +26,19 @@ def get_intersection(f,s):
         dist_x = abs(max_x-min_x)
         dist_y = abs(max_y-min_y)
         inter_box = patches.Rectangle((min_x,min_y),dist_x,dist_y,linewidth=1,fill=True,color='Red')
-        #print(inter_box.get_verts())
         return inter_box
     return None
 
-# TEMP: Should extract the info from contractions in ibex
-def read_data(fig,ax):
+# TODO
+def read_data():
+    '''
+    Reads data on file and returns a single patches.Rectangle as main box and a collection of patches.Rectangle's 
+    '''
     # Box to contract
     box = patches.Rectangle((-9,-9),18,18,linewidth=1,edgecolor='r', fill = None)
-    # Add the patch to the Axes
-    ax.add_patch(box)
-
     this_boxes = []
+
+    # TEMP: Should extract the info from contractions in ibex
     for a in range(0,3):
         x_0 = randint(-9,9)
         y_0 = randint(-9,9)
@@ -43,21 +46,21 @@ def read_data(fig,ax):
         y_length = randint(1,10)
         internal_rect = patches.Rectangle((x_0,y_0),x_length,y_length,linewidth=1,edgecolor='b', facecolor='none')
         this_boxes.append(internal_rect)
-    return this_boxes
-
+    
+    return box,this_boxes
 
 def main():
     # Create figure and axes
     fig,ax = plt.subplots(1)
     ax.grid(True, linestyle='dashed')
     ax.tick_params(labelcolor='r', labelsize='medium', width=1)
-
-    # Plot fixed size
+    # Fix size
     plt.xlim(-10,10)
     plt.ylim(-10,10)
 
-    # REVIEW: 
-    boxes = read_data(fig,ax)
+    box,boxes = read_data()
+    # Add the patch to the Axes
+    ax.add_patch(box)
     for f in boxes:
         for s in boxes:
             if f != s:
@@ -65,6 +68,7 @@ def main():
                 if inter:
                     ax.add_patch(inter)
 
+    # Show patches in plot.
     for b in boxes:
         ax.add_patch(b)
     plt.show()
