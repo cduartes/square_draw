1b37bb644fd9dd50497fd8af1367b80b9fe974bb
ENHANCED\cduartes <c.duartes.dn@gmail.com>
2018-06-08 08:40:57 -0400

    (issued) contracted box

--- plot_shape.py
+++ plot_shape.py
@@ -5,7 +5,7 @@ from random import randint
 from matplotlib.path import Path
 import math
 
-def get_intersection(f,s):
+def intersect(f,s):
     '''
     Return a new patches.Rectangle with the intersection between two patches.Rectangle (f and s parameters) there's one.
     '''
@@ -25,9 +25,28 @@ def get_intersection(f,s):
             max_y = min(max_y,b.get_verts()[2][1])
         dist_x = abs(max_x-min_x)
         dist_y = abs(max_y-min_y)
-        inter_box = patches.Rectangle((min_x,min_y),dist_x,dist_y,linewidth=1,fill=True,color='Red')
+        inter_box = patches.Rectangle((min_x,min_y),dist_x,dist_y,linewidth=1,fill=False,color='Red')
         return inter_box
     return None
+
+def contract_box(this_boxes):
+    '''
+    Return a new patches.Rectangle that contains all intersections
+    '''
+    max_x = float("-inf")
+    max_y = float("-inf")
+    min_x = float("inf")
+    min_y = float("inf")
+    
+    for b in this_boxes:
+        max_x = max(max_x,patches.Rectangle(b).get_verts()[2][0])
+        max_y = max(max_y,b.get_verts()[2][1])
+        min_x = min(min_x,b.get_verts()[0][0])
+        min_y = min(min_y,b.get_verts()[0][1])
+    dist_x = abs(max_x-min_x)
+    dist_y = abs(max_y-min_y)
+    contracted = patches.Rectangle((min_x,min_y),dist_x,dist_y,linewidth=1,fill=True,color='Blue')
+    return contracted
 
 # TODO
 def read_data():
@@ -38,15 +57,24 @@ def read_data():
     box = patches.Rectangle((-9,-9),18,18,linewidth=1,edgecolor='r', fill = None)
     this_boxes = []
 
+    """
     # TEMP: Should extract the info from contractions in ibex
     for a in range(0,3):
         x_0 = randint(-9,9)
         y_0 = randint(-9,9)
         x_length = randint(1,10)
         y_length = randint(1,10)
-        internal_rect = patches.Rectangle((x_0,y_0),x_length,y_length,linewidth=1,edgecolor='b', facecolor='none')
+        internal_rect = patches.Rectangle((x_0,y_0),x_length,y_length,linewidth=1,fill=False)
         this_boxes.append(internal_rect)
-    
+    """
+    rect1 = patches.Rectangle((2,2),5,2,linewidth=1,fill=False)
+    rect2 = patches.Rectangle((5,1),2,4,linewidth=1,fill=False)
+    rect3 = patches.Rectangle((1,1),5,3,linewidth=1,fill=False)
+    this_boxes.append(rect1)
+    this_boxes.append(rect2)
+    this_boxes.append(rect3)
+
+
     return box,this_boxes
 
 def main():
@@ -59,18 +87,21 @@ def main():
     plt.ylim(-10,10)
 
     box,boxes = read_data()
+    intersections = []
     # Add the patch to the Axes
     ax.add_patch(box)
     for f in boxes:
         for s in boxes:
             if f != s:
-                inter = get_intersection(f,s)
-                if inter:
-                    ax.add_patch(inter)
-
+                intersection = intersect(f,s)
+                print(intersection.get_verts())
+                if intersection:
+                    ax.add_patch(intersection)
+                    intersections.append(intersection)
     # Show patches in plot.
     for b in boxes:
         ax.add_patch(b)
+    ax.add_patch(contract_box(intersections))
     plt.show()
 
 if __name__ == '__main__':
